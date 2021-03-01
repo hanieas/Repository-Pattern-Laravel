2082db4f8f9801200b0403eeb2977aa79ad6874e
hanie <hanieasemi76@gmail.com>
2021-03-01 21:22:32 +0330

    register interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\CategoryRepositoryInterface;
+use App\Repositories\Eloquent\BaseRepository;
+use App\Repositories\Eloquent\CategoryRepository;
+use App\Repositories\Eloquent\MarketRepository;
+use App\Repositories\Eloquent\ProductRepository;
+use App\Repositories\EloquentRepositoryInterface;
+use App\Repositories\MarketRepositoryInterface;
+use App\Repositories\ProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(EloquentRepositoryInterface::class,BaseRepository::class);
+        $this->app->bind(MarketRepositoryInterface::class,MarketRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class,ProductRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class,CategoryRepository::class);
     }
 
     /**
